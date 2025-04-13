3a03dd83d9087b6c557bacf1f6adb500f5f8935c
arungoud66 <arungoudchinnamgar@lewisu.edu>
2025-04-13 16:56:16 +0530

    Third Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,10 @@ import Register from './views/Register'
 import Profile from './views/Profile'
 import CollectionPage from './views/CollectionPage'
 import ProductDetails from './components/Products/ProductDetails'
+import Checkout from './components/Cart/CheckOut'
+import OrderConfirmationPage from './views/OrderConfirmationPage'
+import OrderDetailsPage from './views/OrderDetailsPage'
+import MyOrdersPage from './views/MyOrdersPage'
 
 function App() {
 
@@ -23,6 +27,10 @@ function App() {
               <Route path='/profile' element={<Profile />} />
               <Route path='/collections/:collection' element={<CollectionPage />} />
               <Route path='/product/:id' element={<ProductDetails />} />
+              <Route path='/Checkout' element={<Checkout />} />
+              <Route path='/order-confirmation' element={<OrderConfirmationPage />} />
+              <Route path='/order/:id' element={<OrderDetailsPage />} />
+              <Route path='/my-orders' element={<MyOrdersPage />} />
             </Route>
 
           </Routes>
